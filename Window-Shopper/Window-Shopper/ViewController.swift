4eab1f7b959b5738e6e43b2f0c9c5ec25ff51dc5
debratton <debratton@gmail.com>
2018-09-29 13:07:46 -0500

    Base App

---
+++ Window-Shopper/Window-Shopper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Window-Shopper
+//
+//  Created by David E Bratton on 9/29/18.
+//  Copyright © 2018 David Bratton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
